bcaba3a71635936abafc4c1e201c4ea972839ba3
tanishq <tppokhari@gmail.com>
2024-06-13 01:29:31 +0530

    sign up screen basemodel

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,107 @@
+import 'dart:ui';
+
+import 'package:blur/blur.dart';
+import 'package:campus_app/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:glass/glass.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Container(
+              child: ImageFiltered(
+            imageFilter: ImageFilter.blur(
+                sigmaX: 6, sigmaY: 6, tileMode: TileMode.mirror),
+            child: Image.asset("assets/Vector1.png")
+                .blurred(colorOpacity: 0.5, blurColor: Colors.black26),
+          )),
+        ),
+        Scaffold(
+          backgroundColor: Colors.indigo.shade900.withOpacity(0.1),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/Logo.png"),
+              Container(
+                margin: EdgeInsets.symmetric(
+                    vertical: context.responsiveSize(15),
+                    horizontal: context.responsiveSize(40)),
+                child: TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
+                            hintText: "SRM Email / Net ID",
+                            hintStyle: TextStyle(color: Colors.white54),
+                            border:
+                                OutlineInputBorder(borderSide: BorderSide.none),
+                            focusedBorder:
+                                OutlineInputBorder(borderSide: BorderSide.none),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide.none)))
+                    .asGlass(
+                        tintColor: Colors.indigo.shade400,
+                        clipBorderRadius:
+                            BorderRadius.circular(context.responsiveSize(20))),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                    vertical: context.responsiveSize(15),
+                    horizontal: context.responsiveSize(40)),
+                child: TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.white54),
+                            border:
+                                OutlineInputBorder(borderSide: BorderSide.none),
+                            focusedBorder:
+                                OutlineInputBorder(borderSide: BorderSide.none),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide.none)))
+                    .asGlass(
+                        tintColor: Colors.indigo.shade400,
+                        clipBorderRadius:
+                            BorderRadius.circular(context.responsiveSize(20))),
+              ),
+              SizedBox(
+                height: context.responsiveSize(20),
+              ),
+              DecoratedBox(
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.circular(context.responsiveSize(10)),
+                    gradient: const LinearGradient(colors: [
+                      Colors.lightBlue,
+                      Colors.indigoAccent,
+                      Color.fromARGB(255, 121, 35, 220)
+                    ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                child: TextButton(
+                    style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                context.responsiveSize(10))),
+                        foregroundColor: Colors.white,
+                        minimumSize: Size(context.responsiveSize(400), 50)),
+                    onPressed: () {},
+                    child: const Text(
+                      "Sign In",
+                      style: TextStyle(fontSize: 20),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
