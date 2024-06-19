bff74fa4df9cb56d7ebc960e4e9014b1c9883832
tanishq <tppokhari@gmail.com>
2024-06-19 18:33:05 +0530

    add ScreenBase to remaining screens

--- lib/screens/attendance_screen.dart
+++ lib/screens/attendance_screen.dart
@@ -1,4 +1,5 @@
 import 'package:campus_app/widgets/attendance_widget.dart';
+import 'package:campus_app/widgets/screen_base.dart';
 import 'package:flutter/material.dart';
 import 'package:campus_app/main.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -10,45 +11,16 @@ class AttendanceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            const BackgroundImage(),
-            Container(
-              height: context.screenHeight,
-              width: context.screenWidth,
-              color: Colors.indigo.shade900.withOpacity(0.1),
-            ),
-            Padding(
-              padding: EdgeInsets.all(context.responsiveSize(8)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: context.responsiveSize(60),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: context.responsiveSize(16)),
-                    child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Image.asset("assets/logo_small.png")),
-                  ),
-                  Container(
-                      margin: EdgeInsets.all(context.responsiveSize(20)),
-                      alignment: Alignment.centerLeft,
-                      child: SvgPicture.asset("assets/attendance.svg")),
-                  Container(
-                    width: context.responsiveSize(400),
-                    height: context.responsiveSize(152),
-                    child: AttendanceWidget(),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+    return ScreenBase(
+      title: "Attendance",
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(context.responsiveSize(20)),
+            child: AttendanceWidget(),
+          )
+        ],
       ),
     );
   }
